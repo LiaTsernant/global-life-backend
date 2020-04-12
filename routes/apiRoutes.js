8a04adc2153f78082daaa4149a5ccb6c561208b0
Yulia Tsernant <yulia.tsernant@d-d.me>
2020-04-12 14:21:18 -0700

    Added verification

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const ctrl = require("../controllers");
+const verifyToken = require('../middleware/verification');
+
 
 //--------------------- "api/v1/..." ---------------------
 
@@ -9,9 +11,9 @@ router.post("/login", ctrl.auth.login);
 router.post("/register", ctrl.auth.register);
 router.delete("/logout", ctrl.auth.logout);
 //--------------------- USER ---------------------
-router.get("/users", ctrl.user.index);
+router.get("/users", verifyToken, ctrl.user.index);
 //--------------------- COUNTRY ---------------------
-router.get("/countries", ctrl.country.index);
+router.get("/countries", verifyToken, ctrl.country.index);
 
 
 module.exports = router;
