abf0b56dba56332b8dbb8f08339b3814ad0d19e5
Yulia Tsernant <yulia.tsernant@d-d.me>
2020-04-13 15:56:43 -0700

    Created user index and show

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,13 +1,24 @@
 const db = require("../models");
 
 const index = (req, res) => {
-    db.User.find({}, (err, foundCities) => {
-      if (err) return res.status(404).json({ status: 404, error: "Cannot find all cities"});
-      
-      res.json(foundCities);
-    });
+  db.User.find({}, (err, foundUsers) => {
+    if (err) return res.status(404).json({ status: 404, error: "Cannot find all users" });
+
+    res.json(foundUsers);
+  });
 };
 
+const show = (req, res) => {
+  db.User.findById(req.params.userId, (err, foundUser) => {
+    if (err) return res.status(404).json({ status: 404, error: "Cannot find a user" });
+
+    res.json(foundUser);
+  });
+};
+
+
+
 module.exports = {
-    index,
+  index,
+  show
 }
