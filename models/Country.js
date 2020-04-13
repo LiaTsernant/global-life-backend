b292dafc279181f080c0040a651b98495e8aef77
Yulia Tsernant <yulia.tsernant@d-d.me>
2020-04-12 21:16:30 -0700

    Changed fields

--- models/Country.js
+++ models/Country.js
@@ -2,12 +2,10 @@ const mongoose = require("mongoose");
 
 const CountrySchema = new mongoose.Schema({
     name: String,
-    resque: {
-        emergency: { required: true, type: Number },
-        police: { required: true, type: Number },
-        firefighters: { required: true, type: Number },
-        crisisHotline: { required: true, type: Number },
-    }
+    emergency: { required: true, type: String },
+    police: { required: true, type: String },
+    firefighters: { required: true, type: String },
+    crisisHotline: { required: true, type: String },
 }, {timestamps: true});
 
 module.exports = mongoose.model("Country", CountrySchema);
