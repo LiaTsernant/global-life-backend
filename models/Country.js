2d150c304ffc9cc372d8c954493f0763d49aeada
Yulia Tsernant <yulia.tsernant@d-d.me>
2020-04-14 17:58:03 -0700

    Changed model's required field to optional

--- models/Country.js
+++ models/Country.js
@@ -5,7 +5,7 @@ const CountrySchema = new mongoose.Schema({
     emergency: { required: true, type: String },
     police: { required: true, type: String },
     firefighters: { required: true, type: String },
-    crisisHotline: { required: true, type: String },
+    crisisHotline: String,
 }, {timestamps: true});
 
 module.exports = mongoose.model("Country", CountrySchema);
