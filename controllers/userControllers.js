37562db91f88d1df81a803291712c9d4a5e45ddb
Yulia Tsernant <yulia.tsernant@d-d.me>
2020-04-16 19:38:40 -0700

    Removed commented out code

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,15 +1,14 @@
 const db = require("../models");
 
 const index = (req, res) => {
-  db.User.find({}, (err, foundUsers) => {
+  db.User.find({}).populate('country', '_id name emergency police firefighters').exec((err, foundUsers) => {
     if (err) return res.status(404).json({ status: 404, error: "Cannot find all users" });
-
     res.json(foundUsers);
   });
 };
 
 const show = (req, res) => {
-  db.User.findById(req.params.userId, (err, foundUser) => {
+  db.User.findById(req.params.userId).populate('country', '_id name emergency police firefighters').exec((err, foundUser) => {
     if (err) return res.status(404).json({ status: 404, error: "Cannot find a user" });
 
     let returnUser = {
@@ -18,26 +17,34 @@ const show = (req, res) => {
       lastName: foundUser.lastName,
       email: foundUser.email,
       address: foundUser.address,
-      contactPerson: foundUser.contactPerson,
+      contactPersonName: foundUser.contactPersonName,
+      contactPersonPhone: foundUser.contactPersonPhone,
       country: foundUser.country,
       photo: foundUser.photo
-    }
+    };
 
     res.json(returnUser);
   });
 };
 
-const destroy = (req, res) => {
-  db.User.findByIdAndDelete(req.params.id, (err, result) => {
-    if (err) return res.status(404).json({ status: 404, error: "Cannot find a user by id and delete" });
+const update = (req, res) => {
+  console.log('UPDATING')
+  db.User.findByIdAndUpdate(req.params.userId, req.body, { new: true }, (err, updatedUser) => {
+    if (err) return res.status(404).json({ status: 404, error: "Cannot find a user by id and update" });
+    res.json(updatedUser);
+  });
+};
 
+const destroy = (req, res) => {
+  db.User.findByIdAndDelete(req.params.userId, (err, result) => {
+    if (err) return res.status(404).json({ status: 404, error: "Cannot find a user by id and delete" });
     res.json(result);
   });
 };
 
-
-
 module.exports = {
   index,
-  show
-}
+  show,
+  update,
+  destroy
+};
