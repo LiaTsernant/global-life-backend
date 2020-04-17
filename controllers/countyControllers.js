3a5bdec8c8ac572735115560f7b51e09ef3bac0c
Yulia Tsernant <yulia.tsernant@d-d.me>
2020-04-16 19:38:03 -0700

    Removed white spaces

--- controllers/countyControllers.js
+++ controllers/countyControllers.js
@@ -3,22 +3,18 @@ const db = require("../models");
 const index = (req, res) => {
     db.Country.find({}, (err, foundCountries) => {
       if (err) return res.status(404).json({ status: 404, error: "Cannot find all countries"});
-      
       res.json(foundCountries);
     });
 };
 
 const show = (req, res) => {
   db.Country.findOne({ name: req.params.countryName }, (err, foundCountry) => {
-    if (err) return res.status(404).json({ status: 404, error: "Cannot find a country`"});
-    
+    if (err) return res.status(404).json({ status: 404, error: "Cannot find a country`"});  
     res.json(foundCountry);
   });
 };
 
-
-
 module.exports = {
-    index,
-    show,
-}
+  index,
+  show,
+};
