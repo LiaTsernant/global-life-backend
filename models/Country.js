6cf1edd5ebe6d429821d9712692d6461113446de
Yulia Tsernant <yulia.tsernant@d-d.me>
2020-04-16 19:40:04 -0700

    Fixed style

--- models/Country.js
+++ models/Country.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
 const CountrySchema = new mongoose.Schema({
-    name: String,
-    emergency: { required: true, type: String },
-    police: { required: true, type: String },
-    firefighters: { required: true, type: String },
-    crisisHotline: String,
-}, {timestamps: true});
+  name: String,
+  emergency: { required: true, type: String },
+  police: { required: true, type: String },
+  firefighters: { required: true, type: String },
+  crisisHotline: String,
+}, { timestamps: true });
 
 module.exports = mongoose.model("Country", CountrySchema);
