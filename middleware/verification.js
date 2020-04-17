157b6d6b729559fd5beb548d6f0e6bcde6af6a3f
Yulia Tsernant <yulia.tsernant@d-d.me>
2020-04-16 19:39:22 -0700

    Fixed style

--- middleware/verification.js
+++ middleware/verification.js
@@ -5,11 +5,11 @@ const verifyToken = (req, res, next) => {
   jwt.verify(
     token, process.env.JWT_SECRET,
     (err, decodedUser) => {
-      if (err || !decodedUser) return res.status(401).json({ error: 'You are not authorized to do that.'});
+      if (err || !decodedUser) return res.status(401).json({ error: 'You need to be authorized.' });
       req.user = decodedUser;
       next();
     }
-  )
-}
+  );
+};
 
 module.exports = verifyToken;
