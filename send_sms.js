29ca900ff00020dc0c4074f6e00c3a173abb9dd5
Yulia Tsernant <yulia.tsernant@d-d.me>
2020-04-24 11:45:34 -0700

    Removed comments

--- send_sms.js
+++ send_sms.js
@@ -3,8 +3,4 @@ const accountSid = process.env.ACCOUNT_SID;
 const authToken = process.env.AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 
-// client.messages
-//   .create(messageToSend)
-//   .then(message => console.log(message.sid));
-
 module.exports = client;
